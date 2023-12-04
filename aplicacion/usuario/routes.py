b8e49501e25f134a5230f68729f6a07dbfdbe71d
Daniel Perez <daniel101.22v@gmail.com>
2023-12-03 22:00:38 -0500

    Paginacion de la tabla usuarios

--- aplicacion/usuario/routes.py
+++ aplicacion/usuario/routes.py
@@ -18,7 +18,14 @@ from flask_login import login_user, login_manager, logout_user, login_required
 def usuarioPage():
     form = UsuarioForm()
     usuarios = UserModel.listar_usuarios()
-    return render_template('usuario.html', form=form, usuarios=usuarios)
+
+    page = request.args.get('page', 1, type=int)
+    per_page = 5
+
+    total_pages = (len(usuarios) + per_page - 1) // per_page
+    items_on_page = usuarios[(page - 1) * per_page: page * per_page]
+
+    return render_template(template_name_or_list='usuario.html', form=form, usuarios=usuarios, items_on_page=items_on_page, total_pages=total_pages, page=page, mostrar_modal=form.errors)
 
 @usuario.route('/ingresar_usuario', methods=['POST'])
 @login_required
